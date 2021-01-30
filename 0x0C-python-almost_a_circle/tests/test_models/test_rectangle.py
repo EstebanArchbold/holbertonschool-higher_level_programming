0193ffb917635e56075d9a35e8df5846b5b38b8e
EstebanArchbold <est.archbold.edas@gmail.com>
2021-01-29 23:45:41 -0500

    deleting

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Rectangle unit testing"""
-import unittest
-import pep8
-import inspect
-import json
-from models.rectangle import Rectangle
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Square unit testing"""
-import unittest
-import pep8
-import inspect
-import json
-from models.square import Square
